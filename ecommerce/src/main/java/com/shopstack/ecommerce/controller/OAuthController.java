3a538e9b52604bfe119b5a0ca3828897d068700d
Navdha S P <241158.cs@rmkec.ac.in>
2026-07-07 21:24:42 +0530

    Add OAuthController

---
+++ ecommerce/src/main/java/com/shopstack/ecommerce/controller/OAuthController.java
@@ -0,0 +1,14 @@
+package com.shopstack.ecommerce.controller;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OAuthController {
+
+    @GetMapping("/oauth2/user")
+    public Object user(Authentication authentication) {
+        return authentication;
+    }
+}
